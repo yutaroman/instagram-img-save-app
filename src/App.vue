e976dd1e38afd6a1920fea00c17563a5362c894e
yutaro <yutaro.21.com@gmail.com>
2018-11-04 08:31:02 +0900

    Add : minimum function

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="app" id="app">
+        <div class="l-Header">
+            <h1 class="h-Logo">Instagramの画像を保存するアプリケーション</h1>
+        </div>
+        <div class="l-Main">
+            <div class="l-Main_Primary">
+                <form class="p-inputForm">
+                    <input type="text"
+                           name="instagram-url"
+                           v-model="inputUrl"
+                           placeholder="urlを入力してください"
+                           autocomplete="off">
+                    <button type="button"
+                            name="rewrite-button"
+                            v-on:click="clickHandler(inputUrl)">生成する</button>
+                </form>
+                <p class="p-GenerateImage">
+                    <img v-bind:src="url" alt="">
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            url: '',
+            inputUrl: '',
+        }
+    },
+    methods: {
+        clickHandler: function(inputUrl) {
+            this.url = this.inputUrl
+        }
+    }
 }
 </script>
 
 <style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
