909ae06b8d9410cdcdc2949b2d3f94dd91e4127d
yutaro <yutaro.21.com@gmail.com>
2019-03-28 13:03:08 +0900

    Update : LayoutMain の子コンポーネントを単一コンポーネント化

--- src/components/LayoutMain.vue
+++ src/components/LayoutMain.vue
@@ -2,34 +2,13 @@
     <main class="l-Main">
         <div class="l-Main_Container">
             <div class="l-Main_Primary">
-                <div class="p-inputForm">
-                    <input
-                        type="text"
-                        name="instagram-url"
-                        v-model="inputUrl"
-                        placeholder="コピーしたリンクを貼り付け"
-                        autocomplete="off">
-                    <div class="p-inputForm-button">
-                        <button
-                            type="button"
-                            name="rewrite-button"
-                            v-on:click="setGenerateUrl(inputUrl)">生成</button>
-                        <button
-                            type="button"
-                            name="reset-button"
-                            v-on:click="setResetUrl()">リセット</button>
-                    </div>
-                </div>
-                <p class="p-GenerateImage">
-                    <img
-                        v-if="imageUrl === ''"
-                        src="../assets/images/dummy.png"
-                        alt="ダミー画像">
-                    <img
-                        v-else
-                        v-bind:src="imageUrl"
-                        alt="">
-                </p>
+                <primary-input-form
+                    v-bind:inputUrl="inputUrl"
+                    v-bind:imageUrl="imageUrl"
+                    v-on:generate-event="generateUrl"
+                    v-on:reset-event="resetUrl"></primary-input-form>
+                <primary-generate-image
+                    v-bind:imageUrl="imageUrl"></primary-generate-image>
             </div>
         </div>
     </main>
@@ -37,6 +16,8 @@
 
 
 <script>
+    import PrimaryInputForm from './parts/PrimaryInputForm.vue';
+    import PrimaryGenerateImage from './parts/PrimaryGenerateImage.vue';
     const ADD_PARAM = 'media/?size=l';
     export default {
         name: "layout-main",
@@ -46,29 +27,30 @@
                 inputUrl: '',
             }
         },
+        components: {
+            PrimaryInputForm,
+            PrimaryGenerateImage,
+        },
         methods: {
-            setGenerateUrl(inputUrl) {
-                let url = this.inputUrl;
-
+            generateUrl(inputedUrl) {
+                let url = inputedUrl;
                 if (url !== '') {
                     // パラメータが入っていれば削除
                     if (url.indexOf('?')) {
                         url = url.replace(/\?.*$/,"");
                     }
-
                     // 末尾にスラッシュが入ってなければ追加
                     if (url.substr(-1) !== '/') {
                         url = `${url}/`;
                     }
-
-                    // imgタグに bind する変数を変更する
+                    // img タグに bind する変数を変更する
                     this.imageUrl = url + ADD_PARAM;
                 }
             },
-            setResetUrl() {
+            resetUrl() {
                 this.imageUrl = '';
                 this.inputUrl = '';
-            }
+            },
         }
     }
 </script>
@@ -87,75 +69,6 @@
     .l-Main_Primary {
         > :first-child {
             margin-top: 0;
-        }
-    }
-    .p-inputForm {
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        margin-top: 30px;
-        input,
-        button {
-            margin: 0;
-            padding: 0;
-            background: none;
-            border: none;
-            border-radius: 0;
-            appearance: none;
-        }
-        [name="instagram-url"] {
-            background-image: url("../assets/images/icon-search.png");
-            background-repeat: no-repeat;
-            background-size: 10px;
-            background-position: left 10px center;
-            box-sizing: border-box;
-            border: 1px solid #dbdbdb;
-            border-radius: 3px;
-            text-align: center;
-            width: 75%;
-            height: 30px;
-            padding: 0 30px;
-        }
-        [name="rewrite-button"] {
-            box-sizing: border-box;
-            border-radius: 3px;
-            border: 1px solid #dbdbdb;
-            text-align: center;
-            color: #262626;
-            font-size: 1.3rem;
-            font-weight: bold;
-            width: 20%;
-            height: 30px;
-            cursor: pointer;
-        }
-        [name="reset-button"] {
-            box-sizing: border-box;
-            border-radius: 3px;
-            border: 1px solid #dbdbdb;
-            text-align: center;
-            color: #262626;
-            font-size: 1.3rem;
-            font-weight: bold;
-            width: 20%;
-            height: 30px;
-            margin-left: 15px;
-            cursor: pointer;
-        }
-    }
-    .p-inputForm-button {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        margin-top: 15px;
-    }
-    .p-GenerateImage {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-top: 30px;
-        img {
-            border: solid 1px #dbdbdb;
-            max-height: 500px;
         }
     }
 </style>
---
+++ src/components/parts/PrimaryInputForm.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="p-inputForm">
+        <input
+            type="text"
+            name="instagram-url"
+            v-model="url"
+            placeholder="コピーしたリンクを貼り付け"
+            autocomplete="off">
+        <div class="p-inputForm-button">
+            <button
+                type="button"
+                name="rewrite-button"
+                v-on:click="generateHandler()">生成</button>
+            <button
+                type="button"
+                name="reset-button"
+                v-on:click="ResetHandler()">リセット</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'primary-input-form',
+        props: {
+            inputUrl: String,
+            imageUrl: String,
+        },
+        data() {
+            return {
+                url: '',
+            }
+        },
+        methods: {
+            generateHandler() {
+                this.$emit('generate-event', this.url);
+            },
+            ResetHandler() {
+                this.$emit('reset-event');
+            },
+        }
+    }
+</script>
+
+
+<style lang="scss">
+    .p-inputForm {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin-top: 30px;
+        input,
+        button {
+            margin: 0;
+            padding: 0;
+            background: none;
+            border: none;
+            border-radius: 0;
+            appearance: none;
+        }
+        [name="instagram-url"] {
+            background-image: url("../../assets/images/icon-search.png");
+            background-repeat: no-repeat;
+            background-size: 10px;
+            background-position: left 10px center;
+            box-sizing: border-box;
+            border: 1px solid #dbdbdb;
+            border-radius: 3px;
+            text-align: center;
+            width: 75%;
+            height: 30px;
+            padding: 0 30px;
+        }
+        [name="rewrite-button"] {
+            box-sizing: border-box;
+            border-radius: 3px;
+            border: 1px solid #dbdbdb;
+            text-align: center;
+            color: #262626;
+            font-size: 1.3rem;
+            font-weight: bold;
+            width: 20%;
+            height: 30px;
+            cursor: pointer;
+        }
+        [name="reset-button"] {
+            box-sizing: border-box;
+            border-radius: 3px;
+            border: 1px solid #dbdbdb;
+            text-align: center;
+            color: #262626;
+            font-size: 1.3rem;
+            font-weight: bold;
+            width: 20%;
+            height: 30px;
+            margin-left: 15px;
+            cursor: pointer;
+        }
+    }
+    .p-inputForm-button {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        margin-top: 15px;
+    }
+</style>
